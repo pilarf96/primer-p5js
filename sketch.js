059cf52753eafd08eacbff69f052b2affecc0bfd
pilarf96 <133172441+pilarf96@users.noreply.github.com>
2023-07-04 11:23:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,41 @@
-function setup() {
-    createCanvas(512, 512);
-    background(220)
-    frameRate(100)   
-    
-   }
-     
+ function setup() {
+  createCanvas(512, 512);
+  background(220)
+  frameRate(200)   
   
-   function draw() {
-  
-      if (keyIsPressed){
-      background(0)
-  
-    }
-     
-     noFill()
-     stroke(5)
-     strokeWeight(1)
-    circle(mouseX, mouseY, pmouseX, pmouseY);
-      noFill()
-      stroke(0)
-      strokeWeight(2)
-      rect(mouseX, mouseY, pmouseX, pmouseY);
-     noFill()
-     stroke(1)
-     strokeWeight(0.5)
-     ellipse(pmouseX, pmouseY, mouseX, mouseY);
-     
-    
+ }
+   
+
+ function draw() {
+
+
+    if (keyIsPressed){
+    background(0)
+
   }
-  
-     
-  function mouseMoved() {
-    // Cambia el color de fondo cuando el mouse se mueve
-    background(random(255), random(255), random(255));
-  
-   }
+   //si apreto el mouse se dibujan las figuras en el espacio, y queda un solo color de fondo. Si dejo de apretar se borran. 
+   noFill()
+   stroke(255, 255, 255)
+   strokeWeight(0.5)
+  circle(mouseX, mouseY, pmouseX); 
+    noFill()
+     stroke(255, 255, 255)
+    strokeWeight(1)
+    rect(mouseX, mouseY, pmouseX, pmouseY);
+   noFill()
+   stroke(255, 255, 255)
+   strokeWeight(0.2)
+   ellipse(pmouseX, pmouseY, mouseX, mouseY);
+    noFill()
+     stroke(255, 255, 255)
+    strokeWeight(1)
+    rect(pmouseX, pmouseY, mouseX, mouseY);
+   
+}
+
+   
+function mouseMoved() {
+  // Cambia el color de fondo cuando el mouse se mueve
+  background(random(100));
+
+ }
